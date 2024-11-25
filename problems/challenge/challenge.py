3ce01fa9cc6609a520ec82d40ee9ff58d799e69f
samylokanandi <67350400+samylokanandi@users.noreply.github.com>
2024-11-25 14:14:49 -0500

    Update challenge.py

--- problems/challenge/challenge.py
+++ problems/challenge/challenge.py
@@ -2,5 +2,15 @@
 Challenge 1
 '''
 
-def find_max_form(strs, m, n): 
-    pass
+def find_max_form(strs, m, n):
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    for s in strs:
+        zeros = s.count('0')
+        ones = s.count('1')
+        
+        for i in range(m, zeros - 1, -1):
+            for j in range(n, ones - 1, -1):
+                dp[i][j] = max(dp[i][j], 1 + dp[i - zeros][j - ones])
+    
+    return dp[m][n]
